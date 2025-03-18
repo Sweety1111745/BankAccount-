59b0667aa163adee4cee24b52ce93ea55cdc8ae4
Романенков Никита  Александрович <322123@college>
2025-03-18 10:13:14 +0300

    нулевой

--- BankAccountApp/obj/Debug/net8.0/BankAccountApp.AssemblyInfo.cs
+++ BankAccountApp/obj/Debug/net8.0/BankAccountApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankAccountApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a16d411d457f9eebf39fcdb64dc41c7bf5003856")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankAccountApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankAccountApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
